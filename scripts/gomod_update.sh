71226fe9dda8dbd88c2d6344f13ce3810aef14fb
jiananwen <jianan.wen@hotmail.com>
2021-10-15 16:02:13 +0800

    update gomod_update.sh

--- scripts/gomod_update.sh
+++ scripts/gomod_update.sh
@@ -5,9 +5,8 @@
 set -x
 BRANCH=develop
 
-#go get chainmaker.org/chainmaker/localconf/v2@${BRANCH}
-#go get chainmaker.org/chainmaker/logger/v2@${BRANCH}
-
+go get chainmaker.org/chainmaker/localconf/v2@${BRANCH}
+go get chainmaker.org/chainmaker/logger/v2@${BRANCH}
 go get chainmaker.org/chainmaker/utils/v2@${BRANCH}
 go get chainmaker.org/chainmaker/protocol/v2@${BRANCH}
 go get chainmaker.org/chainmaker/pb-go/v2@${BRANCH}
