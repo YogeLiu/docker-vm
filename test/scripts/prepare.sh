6d97e236aa0b20688dad10e6bb46ace97c9a89f0
jiananwen <jianan.wen@hotmail.com>
2021-11-10 17:03:16 +0800

    update image version

--- test/scripts/prepare.sh
+++ test/scripts/prepare.sh
@@ -3,8 +3,8 @@
 docker_image_name=(`docker images | grep "chainmakerofficial/chainmaker-vm-docker-go"`)
 
 if [ ${docker_image_name} ]; then
-  docker image rm chainmakerofficial/chainmaker-vm-docker-go:develop
+  docker image rm chainmakerofficial/chainmaker-vm-docker-go:v2.1.0
 fi
 
 cd ../../vm_mgr && go mod vendor
-docker build -t chainmakerofficial/chainmaker-vm-docker-go:develop -f Dockerfile ./
+docker build -t chainmakerofficial/chainmaker-vm-docker-go:v2.1.0 -f Dockerfile ./
