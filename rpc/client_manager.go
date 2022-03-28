bee2b9bf70b300e391954e0e02fa4c9d63b8331f
jianan <jianan.wen@hotmail.com>
2022-03-28 19:50:13 +0800

    add comment

--- rpc/client_manager.go
+++ rpc/client_manager.go
@@ -1,3 +1,9 @@
+/*
+Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+Copyright (C) BABEC. All rights reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
 package rpc
 
 import (
