712701d1dd4a7de483763309f88cdb4eecc9c92d
liuxinfeng <liuxinfeng96@163.com>
2022-04-28 16:27:01 +0800

    fix: shell fix

--- test/scripts/prepare.sh
+++ test/scripts/prepare.sh
@@ -7,7 +7,7 @@ VERSION=v2.2.0.1
 
 docker_image_name=`docker images | grep "chainmakerofficial/chainmaker-vm-docker-go"`
 
-if [ ${docker_image_name} ]; then
+if [[ ${docker_image_name} ]]; then
   docker image rm chainmakerofficial/chainmaker-vm-docker-go:${VERSION}
   rm -fr ../testdata/org1
   rm -fr ../testdata/log
