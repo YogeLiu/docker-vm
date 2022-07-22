151626c7f28f32dbc58ba7b6fefbe0679e234df6
guanghonglu <guanghonglu@tencent.com>
2022-07-22 16:12:22 +0800

    feat: Add util time statics       --story=1011788

---
+++ utils/time_statics.go
@@ -0,0 +1,233 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"chainmaker.org/chainmaker/logger/v2"
+	"chainmaker.org/chainmaker/vm-engine/v2/pb/protogo"
+)
+
+type SysCallDuration struct {
+	OpType          protogo.DockerVMType
+	StartTime       int64
+	TotalDuration   int64
+	StorageDuration int64
+}
+
+func NewSysCallDuration(opType protogo.DockerVMType, startTime int64, totalTime int64,
+	storageTime int64) *SysCallDuration {
+	return &SysCallDuration{
+		OpType:          opType,
+		StartTime:       startTime,
+		TotalDuration:   totalTime,
+		StorageDuration: storageTime,
+	}
+}
+
+func (s *SysCallDuration) ToString() string {
+	if s == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s start: %v, spend: %dμs, r/w store: %dμs; ",
+		s.OpType.String(), time.Unix(s.StartTime/1e9, s.StartTime%1e9), s.TotalDuration/1000, s.StorageDuration/1000,
+	)
+}
+
+type TxDuration struct {
+	TxId                      string
+	StartTime                 int64
+	EndTime                   int64
+	TotalDuration             int64
+	SysCallCnt                int32
+	SysCallDuration           int64
+	StorageDuration           int64
+	ContingentSysCallCnt      int32
+	ContingentSysCallDuration int64
+	CrossCallCnt              int32
+	CrossCallDuration         int64
+	SysCallList               []*SysCallDuration
+}
+
+func NewTxDuration(txId string, startTime int64) *TxDuration {
+	return &TxDuration{
+		TxId:      txId,
+		StartTime: startTime,
+	}
+}
+
+func (e *TxDuration) ToString() string {
+	if e == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s spend time: %dμs, syscall: %dμs(%d), r/w store: %dμs, possible syscall: %dμs(%d)"+
+		"cross contract: %dμs(%d)",
+		e.TxId, e.TotalDuration/1000, e.SysCallDuration/1000, e.SysCallCnt, e.StorageDuration/1000,
+		e.ContingentSysCallDuration/1000, e.ContingentSysCallCnt, e.CrossCallDuration/1000, e.CrossCallCnt,
+	)
+}
+
+func (e *TxDuration) PrintSysCallList() string {
+	if e.SysCallList == nil {
+		return "no syscalls"
+	}
+	var sb strings.Builder
+	for _, sysCallTime := range e.SysCallList {
+		sb.WriteString(sysCallTime.ToString())
+	}
+	return sb.String()
+}
+
+// StartSysCall start new sys call
+func (e *TxDuration) StartSysCall(msgType protogo.DockerVMType) {
+	duration := &SysCallDuration{
+		OpType:    msgType,
+		StartTime: time.Now().UnixNano(),
+	}
+	e.SysCallList = append(e.SysCallList, duration)
+}
+
+// AddLatestStorageDuration add storage time to latest sys call
+func (e *TxDuration) AddLatestStorageDuration(duration int64) error {
+	latestSysCall, err := e.GetLatestSysCall()
+	if err != nil {
+		return fmt.Errorf("failed to get latest sys call, %v", err)
+	}
+	latestSysCall.StorageDuration += duration
+	return nil
+}
+
+// EndSysCall close new sys call
+func (e *TxDuration) EndSysCall(msg *protogo.DockerVMMessage) error {
+	latestSysCall, err := e.GetLatestSysCall()
+	if err != nil {
+		return fmt.Errorf("failed to get latest sys call, %v", err)
+	}
+	latestSysCall.TotalDuration = time.Since(time.Unix(0, latestSysCall.StartTime)).Nanoseconds()
+	e.addSysCallDuration(latestSysCall)
+	if latestSysCall.OpType == protogo.DockerVMType_TX_RESPONSE {
+		e.CrossCallCnt = msg.Response.TxDuration.CrossCallCnt
+		e.CrossCallDuration = msg.Response.TxDuration.CrossCallTime
+	}
+	return nil
+}
+
+// GetLatestSysCall returns latest sys call
+func (e *TxDuration) GetLatestSysCall() (*SysCallDuration, error) {
+	if len(e.SysCallList) == 0 {
+		return nil, errors.New("sys call list length == 0")
+	}
+	return e.SysCallList[len(e.SysCallList)-1], nil
+}
+
+// todo add lock (maybe do not need)
+func (e *TxDuration) addSysCallDuration(duration *SysCallDuration) {
+	if duration == nil {
+		return
+	}
+	switch duration.OpType {
+	case protogo.DockerVMType_GET_BYTECODE_REQUEST:
+		e.ContingentSysCallCnt++
+		e.ContingentSysCallDuration += duration.TotalDuration
+		e.StorageDuration += duration.StorageDuration
+	case protogo.DockerVMType_GET_STATE_REQUEST, protogo.DockerVMType_GET_BATCH_STATE_REQUEST,
+		protogo.DockerVMType_CREATE_KV_ITERATOR_REQUEST, protogo.DockerVMType_CONSUME_KV_ITERATOR_REQUEST,
+		protogo.DockerVMType_CREATE_KEY_HISTORY_ITER_REQUEST, protogo.DockerVMType_CONSUME_KEY_HISTORY_ITER_REQUEST,
+		protogo.DockerVMType_GET_SENDER_ADDRESS_REQUEST:
+		e.SysCallCnt++
+		e.SysCallDuration += duration.TotalDuration
+		e.StorageDuration += duration.StorageDuration
+	default:
+		return
+	}
+}
+
+func (e *TxDuration) Add(t *TxDuration) {
+	if t == nil {
+		return
+	}
+
+	t.TotalDuration += t.TotalDuration
+
+	e.SysCallCnt += t.SysCallCnt
+	e.SysCallDuration += t.SysCallDuration
+	e.StorageDuration += t.StorageDuration
+
+	e.ContingentSysCallCnt += t.ContingentSysCallCnt
+	e.ContingentSysCallDuration += t.ContingentSysCallDuration
+
+	e.CrossCallCnt += t.CrossCallCnt
+	e.CrossCallDuration += t.CrossCallDuration
+}
+
+func (e *TxDuration) AddContingentSysCall(spend int64) {
+	e.ContingentSysCallCnt++
+	e.ContingentSysCallDuration += spend
+}
+
+type BlockTxsDuration struct {
+	txs []*TxDuration
+}
+
+// todo add lock
+func (b *BlockTxsDuration) AddTxDuration(t *TxDuration) {
+	b.txs = append(b.txs, t)
+}
+
+func (b *BlockTxsDuration) ToString() string {
+	if b == nil {
+		return ""
+	}
+	txTotal := NewTxDuration("", 0)
+	for _, tx := range b.txs {
+		txTotal.Add(tx)
+	}
+	return txTotal.ToString()
+}
+
+type BlockTxsDurationMgr struct {
+	blockDurations map[string]*BlockTxsDuration
+	lock           sync.Mutex
+	logger         *logger.CMLogger
+}
+
+func NewBlockTxsDurationMgr() *BlockTxsDurationMgr {
+	return &BlockTxsDurationMgr{
+		blockDurations: make(map[string]*BlockTxsDuration),
+		logger:         logger.GetLogger(logger.MODULE_RPC),
+	}
+}
+
+func (r *BlockTxsDurationMgr) PrintBlockTxsDuration(id string) string {
+	durations := r.blockDurations[id]
+	return durations.ToString()
+}
+
+func (r *BlockTxsDurationMgr) AddBlockTxsDuration(id string) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	if r.blockDurations[id] == nil {
+		r.blockDurations[id] = &BlockTxsDuration{}
+		return
+	}
+	r.logger.Warnf("receive duplicated block, fingerprint: %s", id)
+}
+
+func (r *BlockTxsDurationMgr) RemoveBlockTxsDuration(id string) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	delete(r.blockDurations, id)
+}
+
+// AddTx if add tx to block map need lock
+func (r *BlockTxsDurationMgr) AddTx(id string, txTime *TxDuration) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	if r.blockDurations[id] == nil {
+		return
+	}
+	r.blockDurations[id].AddTxDuration(txTime)
+}
