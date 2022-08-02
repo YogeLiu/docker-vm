5a5b4a6b62a15d4fe6143423e22caf410b9bfbbd
litenghui <644052732@qq.com>
2022-08-02 11:57:27 +0800

    chore: udpate ut_cover
    --story=1011788

--- ut_cover.sh
+++ ut_cover.sh
@@ -6,7 +6,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 function ut_cover() {
-  go test -v -coverprofile cover.out ./...
+  go test -coverprofile cover.out ./...
   total=$(go tool cover -func=cover.out | tail -1)
   echo ${total}
   rm cover.out
