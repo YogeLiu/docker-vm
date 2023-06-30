bd1e53dafc472318469057b41bbe6c14acb02dbc
Han Tianle <danahan@tencent.com>
2023-06-30 11:01:54 +0800

    feat: Modify image version
    --story=0

--- test/scripts/batch-start.sh
+++ test/scripts/batch-start.sh
@@ -15,7 +15,7 @@ do
     EXPOSE_PORT=$((22351+$i-1))
     RUNTIME_PORT=$((32351+$i-1))
     CONTAINER_NAME=chainmaker-docker-vm-$i
-    IMAGE_NAME="chainmakerofficial/chainmaker-vm-engine:v2.3.1"
+    IMAGE_NAME="chainmakerofficial/chainmaker-vm-engine:v2.3.2"
     
     if  [ ! -d "$MOUNT_PATH" ];then
     mkdir -p "$MOUNT_PATH"
